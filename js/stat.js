b5e40022b1f15a9c40585c21c44bffb3ad18af04
Dmitry Kabanov <dkabanov6@gmail.com>
2017-01-24 09:00:25 +0500

    Респределил код по функциям и попрпавил ошибки линтера

--- js/stat.js
+++ js/stat.js
@@ -1,12 +1,9 @@
-'use strict';  
+'use strict';
 
-var drawCloud = function (ctx, x, y, width, height) {
+var drawCloud = function (ctx, x, y, width, height, background) {
   ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(width, y);
-  ctx.lineTo(width, height);
-  ctx.lineTo(x, height);
-  ctx.lineTo(x, y);
+  ctx.fillStyle = background;
+  ctx.fillRect(x, y, width, height);
   ctx.stroke();
   ctx.closePath();
   ctx.fill();
@@ -22,18 +19,38 @@ var drawline = function (ctx, x, y, height) {
   ctx.fill();
 };
 
-window.renderStatistics = function (ctx, names, times) {
-
-  var histogramWidth = 140;
+var drawColumn = function (ctx, name, time, histogramElementWidth, histogramElementColor, stepBetweenElements) {
   var histogramElementHeight = 30;
-  var step = 80;
-  var maxTime = 0;
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  drawCloud(ctx, 120, 20, 430, 300);
+  ctx.fillStyle = '#000000';
+  ctx.fillText(name, 125, stepBetweenElements - 5);
 
+  if (name === 'Вы') {
+    ctx.fillStyle = 'rgba(235, 16, 16, 0.8)';
+  } else {
+    ctx.fillStyle = histogramElementColor;
+  }
+
+  ctx.fillRect(120, stepBetweenElements, histogramElementWidth, histogramElementHeight);
   ctx.fillStyle = '#ffffff';
-  drawCloud(ctx, 110, 10, 420, 290);
+  ctx.fillText(time.toFixed(1) + 'c.', histogramElementWidth + 70, stepBetweenElements + 20);
+};
+
+var randomColor = function () {
+  var r = (Math.floor(Math.random() * 256)).toString(10);
+  var g = (Math.floor(Math.random() * 256)).toString(10);
+  var b = (Math.floor(Math.random() * 256)).toString(10);
+
+  return 'rgba' + '(' + r + ',' + g + ',' + b + ',' + 0.8 + ')';
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  var histogramWidth = 140;
+  var maxTime = Math.max.apply(null, times);
+  var histogramStep = histogramWidth / (maxTime / 1000);
+
+  drawCloud(ctx, 120, 20, 330, 280, 'rgba(0, 0, 0, 0.7)');
+  drawCloud(ctx, 110, 10, 330, 280, '#ffffff');
 
   drawline(ctx, 120, 70, 285);
   drawline(ctx, 180, 70, 285);
@@ -46,34 +63,19 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 180, 50);
 
   for (var i = 0; i < times.length; i++) {
-    if (times[i] > maxTime) {
-      maxTime = times[i]
-    }
-  }
-
-  var histogramStep = histogramWidth / (maxTime / 1000);
-
-  for (var z = 0; z < times.length; z++) {
-    var name = names[z];
-    var time = times[z] / 1000;
+    var name = names[i];
+    var time = times[i] / 1000;
     var histogramElementWidth = histogramStep * time;
+    var histogramElementColor = randomColor();
+    var stepBetweenElements;
 
-    //Random color
-    var r = (Math.floor(Math.random() * 256)).toString(10);
-    var g = (Math.floor(Math.random() * 256)).toString(10);
-    var b = (Math.floor(Math.random() * 256)).toString(10);
-    var rgba = 'rgba' + '(' + r + ',' + g + ',' + b + ',' + 0.8 + ')';
+    if (i === 0) {
+      stepBetweenElements = 80;
+    } else {
+      stepBetweenElements += 55;
+    }
 
-    ctx.fillStyle = '#000000';
-    ctx.fillText(name, 125, step - 5);
-
-    name === 'Вы' ? ctx.fillStyle = 'rgba(235, 16, 16, 0.8)' : ctx.fillStyle = rgba;
-
-    ctx.fillRect(120, step, histogramElementWidth, histogramElementHeight);
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(time.toFixed(1) + 'c.', histogramElementWidth + 70, step + 20);
-
-    step += 55;
+    drawColumn(ctx, name, time, histogramElementWidth, histogramElementColor, stepBetweenElements);
   }
-};  
+};
 
