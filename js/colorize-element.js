9ee2fc18a131f51dc39e920230a4b1a869bcc794
Dmitry Kabanov <dkabanov6@gmail.com>
2017-02-13 21:56:59 +0500

    Инкапсулировал функции в модулях

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,18 +1,20 @@
 'use strict';
 
-window.colorizeElement = function (element, colors, property, eventType) {
-  var currentColor = colors[0];
+window.colorizeElement = (function () {
 
-  element.addEventListener(eventType, function (e) {
+  return function (element, colors, property, eventType) {
+    var currentColor = colors[0];
 
-    if (e.type === 'click' || window.utils.isActiveEvent(e)) {
-      var newColor = null;
-      while (!newColor || newColor === currentColor) {
-        newColor = window.utils.getRandomElementExcept(colors, currentColor);
+    element.addEventListener(eventType, function (e) {
+
+      if (e.type === 'click' || window.utils.isActiveEvent(e)) {
+        var newColor = null;
+        while (!newColor || newColor === currentColor) {
+          newColor = window.utils.getRandomElementExcept(colors, currentColor);
+        }
+        currentColor = newColor;
+        element.style[property] = currentColor;
       }
-      currentColor = newColor;
-      element.style[property] = currentColor;
-    }
-
-  });
-};
+    });
+  };
+})();
--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,24 @@
 'use strict';
 
-window.utils = {
-  KEY_CODE_ENTER: 13,
-  getRandomElement: function (array) {
+window.utils = (function () {
+  var KEY_CODE_ENTER = 13;
+
+  var getRandomElement = function (array) {
     var randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
-  },
-  getRandomElementExcept: function (array, elementArray) {
-    var randomElement = null;
-    while (!randomElement || randomElement === elementArray) {
-      randomElement = this.getRandomElement(array);
+  };
+
+  return {
+    getRandomElementExcept: function (array, elementArray) {
+      var randomElement = null;
+      while (!randomElement || randomElement === elementArray) {
+        randomElement = getRandomElement(array);
+      }
+      return randomElement;
+    },
+
+    isActiveEvent: function (e) {
+      return e.keyCode && e.keyCode === KEY_CODE_ENTER;
     }
-    return randomElement;
-  },
-  isActiveEvent: function (e) {
-    return e.keyCode && e.keyCode === this.KEY_CODE_ENTER;
-  }
-};
+  };
+})();
